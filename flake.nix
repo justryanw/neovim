a704170e9ae78e7eb4f84b66e8ec8765e195e10f
Ryan Walker <ryanjwalker2001@gmail.com>
2024-06-23 10:39:53 +0100

    shell

--- flake.nix
+++ flake.nix
@@ -33,14 +33,12 @@
           nvim = nixvim'.makeNixvimWithModule nixvimModule;
         in
         {
-          checks = {
-            # Run `nix flake check .` to verify that your config is not broken
-            default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
-          };
+          checks.default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
 
-          packages = {
-            # Lets you run `nix run .` to start nixvim
-            default = nvim;
+          packages.default = nvim;
+
+          devShells.default = pkgs.mkShell {
+            buildInputs = [ nvim ];
           };
         };
     };
