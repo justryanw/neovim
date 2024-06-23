9be43fd696933ae8a49b6cf9bd468f9fa2052a7a
Ryan Walker <ryanjwalker2001@gmail.com>
2024-06-23 11:13:30 +0100

    basic

--- config/default.nix
+++ config/default.nix
@@ -1,4 +1,28 @@
 {
   # Import all your configuration modules here
   imports = [ ./bufferline.nix ];
+
+  options = {
+    number = true;
+    relativenumber = true;
+    shiftwidth = 2; # Tab width should be 2
+  };
+
+  config = {
+    colorschemes.onedark.enable = true;
+
+    plugins.lsp = {
+      enable = true;
+
+      servers = {
+        rust-analyzer = {
+	  enable = true;
+	  installCargo = false;
+	  installRustc = false;
+	};
+
+	nixd.enable = true;
+      };
+    };
+  };
 }
